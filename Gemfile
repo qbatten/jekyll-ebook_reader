62ae4c8fc864d7030a3b012bf339ea04ac5d1617
Quinn Batten <quinnbatten@mac.com>
2021-02-13 09:47:48 -0500

    initial attempt...

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in jekyll-ebook_reader.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "rubocop", "~> 1.7"
---
+++ lib/jekyll/ebook_reader.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ebook_reader/version"
+
+module Jekyll
+  module EbookReader
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
